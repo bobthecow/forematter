17631f1064e9d8c1d3125038fd38cd24097fcebf
Justin Hileman <justin@justinhileman.info>
2013-12-31 10:45:58 -0800

    Update version output (consistent with git, etc)

--- lib/forematter/commands/fore.rb
+++ lib/forematter/commands/fore.rb
@@ -8,12 +8,8 @@ flag :h, :help, 'show the help message and quit' do |value, cmd|
   exit 0
 end
 
-# flag :V, :verbose, 'enable verbose output' do
-#   Forematter.verbose = true
-# end
-
 flag :v, :version, 'show version' do
-  puts Forematter::VERSION
+  puts "fore version #{Forematter::VERSION}"
   exit 0
 end
 
